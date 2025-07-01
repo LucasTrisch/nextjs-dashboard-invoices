d917243800e80c3cf5ee2c85c1b63fe415b32d08
lucas_t_de-almeida <lucastrisch6@gmail.com>
2025-07-01 20:47:11 -0300

    oi

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,18 @@
-export default function Page(){
-    return <p>Dashboard Page</p>
+import { lusitana } from "@/app/ui/fonts";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import { revenue } from "../lib/placeholder-data";
+
+export default async function Page(){
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grind gap-6 sm:grind-cols-2 lg:grind-cols-4">
+                </div>
+                <div className="mt-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-8">
+                 <RevenueChart revenue={revenue}/>   
+                    </div>
+        </main>
+    )
 }
