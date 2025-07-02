2d9b55969fda9fd64cb27efe5e187a5bc63a3bc4
lucas_t_de-almeida <lucastrisch6@gmail.com>
2025-07-01 21:22:58 -0300

    oi

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import { lusitana } from "@/app/ui/fonts";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import { revenue } from "../lib/placeholder-data";
+import { fetchLatestInvoices } from "../lib/data";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 
 export default async function Page(){
+    const latestInvoices = await fetchLatestInvoices();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -11,7 +14,8 @@ export default async function Page(){
             <div className="grind gap-6 sm:grind-cols-2 lg:grind-cols-4">
                 </div>
                 <div className="mt-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-8">
-                 <RevenueChart revenue={revenue}/>   
+                 <RevenueChart revenue={revenue}/> 
+                    <LatestInvoices latestInvoices={latestInvoices} />  
                     </div>
         </main>
     )
